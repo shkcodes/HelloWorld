5ae45d0f50c555d22076fa626b8d95df64f9688f
Shashank Mishra <shashank@fueled.com>
2019-02-20 21:27:25 +0530

    toast!!

--- app/src/main/java/com/example/shashank/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/shashank/helloworld/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Get this back in", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Get this back in!", Toast.LENGTH_SHORT).show()
     }
 }
