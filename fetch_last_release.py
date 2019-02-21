fbd2f05bbcd00c627c19fcc4010a823dc9034c7d
Shashank Mishra <shashank@fueled.com>
2019-02-21 21:11:34 +0530

    python script to download last apk

--- fetch_last_release.py
+++ fetch_last_release.py
@@ -4,7 +4,6 @@ import urllib
 import os
 
 response = requests.get('https://api.github.com/repos/shkcodes/HelloWorld/releases/latest', headers={'Authorization': 'token '+ os.environ["GITHUB_API_TOKEN"]})
-print(os.environ["GITHUB_API_TOKEN"])
 model = json.loads(response.text)
 downloadUrl = model["assets"][0]["browser_download_url"]
 urllib.urlretrieve(downloadUrl, "old.apk")
