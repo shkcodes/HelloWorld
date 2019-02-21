92c770bd92ae01a84c0ae22c7e0c51931d623071
Shashank Mishra <shashank@fueled.com>
2019-02-21 18:48:50 +0530

    testing python script

--- py.py
+++ py.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import requests
+import json
+import urllib
+import os
+
+response = requests.get('https://api.github.com/repos/shkcodes/HelloWorld/releases/latest', headers={'Authorization': 'token '+ os.environ["GITHUB_API_TOKEN"]})
+print(os.environ["GITHUB_API_TOKEN"])
+model = json.loads(response.text)
+downloadUrl = model["assets"][0]["browser_download_url"]
+urllib.urlretrieve(downloadUrl, "old.apk")
+print(downloadUrl)
