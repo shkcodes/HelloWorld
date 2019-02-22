2909dac4e2d7354165c4e088a3ec377d9d639745
Shashank Mishra <shashank@fueled.com>
2019-02-22 22:50:54 +0530

    show apk diff table

--- post_comment.py
+++ post_comment.py
@@ -2,16 +2,42 @@ import json
 import requests
 import os
 from itertools import islice
+from tabulate import tabulate
 
 user = os.environ["CIRCLE_PROJECT_USERNAME"]
 project = os.environ["CIRCLE_PROJECT_REPONAME"]
 pr_url = os.environ["CIRCLE_PULL_REQUEST"]
 pr_no = pr_url[pr_url.rfind('/') + 1:]
 body = ""
-with open("apkDiff.txt") as apk_diff:
-    for line in islice(apk_diff, 5):
-        body += line
+lines = open("apkDiff.txt", "r").read().splitlines()
+byteToMBMultiplier = float(1) / (1024 * 1024)
+decimalFormatter = '{0:+.3f}'
 
+classesDexSize = 0
+resSize = 0
+layoutSize = 0
+
+re.split('\t', lines[0])
+
+for line in lines:
+    tabs = re.split('\t', line)
+    if "classes" in tabs[3]:
+        classesDexSize += float(tabs[2])
+    if "/res/" in tabs[3]:
+        resSize += float(tabs[2])
+    if "/res/layout/" in tabs[3]:
+        layoutSize += float(tabs[2])
+
+classesDexSize = str(decimalFormatter.format(classesDexSize * byteToMBMultiplier))
+resSize = str(decimalFormatter.format(resSize * byteToMBMultiplier))
+layoutSize = str(decimalFormatter.format(layoutSize * byteToMBMultiplier))
+apkSize = str(decimalFormatter.format(float(re.split('\t', lines[0])[2]) * byteToMBMultiplier))
+
+body = tabulate([["apk size", apkSize + "MB"],
+                ["dex size", classesDexSize + "MB"],
+                ["res size", resSize + "MB"],
+                ["layout size", layoutSize + "MB"]],
+               tablefmt="grid")
 payload = {
     'body': body
 }
